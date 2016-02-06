1cf64782c87f7916732e28e8bed155dbf907bd4a
dubuapt <kozazz@hanmail.net>
2016-02-06 09:49:27 +0900

    Update

--- wsgi/fiddly/feedlyApiTest.py
+++ wsgi/fiddly/feedlyApiTest.py
@@ -82,7 +82,8 @@ def makeFile(name ,seq ,category):
         # startTimestamp = datetime.datetime.fromtimestamp(item['crawled'][:3]).strftime('%Y-%m-%dT%H:%M:%SZ')
         tmpTimestamp =  str(item['crawled'])[:-3]
         # print(tmpTimestamp)
-        cwTimestamp =datetime.datetime.fromtimestamp(int(tmpTimestamp)).strftime('%Y-%m-%dT%H:%M:%SZ')
+        # cwTimestamp =datetime.datetime.fromtimestamp(int(tmpTimestamp)).strftime('%Y-%m-%dT%H:%M:%SZ')
+        cwTimestamp = dateutil.parser.parse("02-02-2016").strftime('%Y-%m-%dT%H:%M:%SZ')
 
         print_format ("\"startTimestamp\":\"%s\"," %(cwTimestamp))
         print_format ("\"endTimestamp\":\"%s\"," %(cwTimestamp))
