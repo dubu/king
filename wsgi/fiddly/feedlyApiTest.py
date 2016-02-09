45b04b14e273d073ee1d290416d4e2e409077c09
dubuapt <kozazz@hanmail.net>
2016-02-09 22:51:24 +0900

    Update

--- wsgi/fiddly/feedlyApiTest.py
+++ wsgi/fiddly/feedlyApiTest.py
@@ -51,7 +51,6 @@ print(categoryList)
 def makeFile(name ,seq ,category):
 
     def getFileName(name, seq):
-        return name + str(seq) + ".json";
 
         if os.path.isfile(name + str(seq) + ".json"):
             return getFileName(name, seq+1)
