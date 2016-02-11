8917a7899226b553fa0605de50fb332a5816d1af
dubuapt <kozazz@hanmail.net>
2016-02-12 08:06:24 +0900

    Add shrot category

--- wsgi/fiddly/feedlyApiTest.py
+++ wsgi/fiddly/feedlyApiTest.py
@@ -175,5 +175,5 @@ makeFile("../static/short_v",1,cates[3])
 
 
 ff= open("../static/manifest_v1.json", 'w', encoding='utf-8')
-ff.write("{\"format\":\"iosched-json-v1\",\"data_files\":[\"enter_v%d.json\",\"it_v%d.json\",\"life_v%d.json\",\"media_v%d.json\"]}" %(g_seq,g_seq,g_seq,g_seq))
+ff.write("{\"format\":\"iosched-json-v1\",\"data_files\":[\"enter_v%d.json\",\"it_v%d.json\",\"life_v%d.json\",\"media_v%d.json\",\"short_v%d.json\"]}" %(g_seq,g_seq,g_seq,g_seq,g_seq))
 ff.close()
